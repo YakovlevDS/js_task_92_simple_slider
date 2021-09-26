e791e2232ee15ff5434e9d7604dd8754d6811f52
Dmitriy Yakovlev <1d3ddd@gmail.com>
2021-09-26 18:23:03 +0200

    added func

--- app.js
+++ app.js
@@ -2,14 +2,16 @@
 
 
 // Solution 1
-const slides = document.querySelectorAll('.slide')
-for (const slide of slides) {
-    slide.addEventListener('click', () => {
-       slides.forEach(i => i.classList.remove('active'));
-       slide.classList.add('active')
-    })
+function slidesPlugin(activeSlide=0) {
+  const slides = document.querySelectorAll(".slide");
+slides[activeSlide].classList.add('active');
+  for (const slide of slides) {
+    slide.addEventListener("click", () => {
+      slides.forEach((i) => i.classList.remove("active"));
+      slide.classList.add("active");
+    });
+  }
 }
-
-
+slidesPlugin(3);
 
 // ! Explanation: 
